0c95024210d24ca14c6a5db547c78460899e34a6
Manali99mm <a.manalimathur@gmail.com>
2022-08-09 16:47:00 +0530

    changes in add new book modal design

--- src/Components/AddNewBookModal.jsx
+++ src/Components/AddNewBookModal.jsx
@@ -50,8 +50,8 @@ const AddNewBookModal = ({ setShowModal, id, book }) => {
 
     return (
         <>
-            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none ">
-                <div className="relative w-full my-6 mx-auto max-w-2xl">
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 my-6 z-50 outline-none focus:outline-none ">
+                <div className="relative w-full h-full my-6 mx-auto max-w-2xl">
                     {/*content*/}
                     <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                         {/*header*/}
